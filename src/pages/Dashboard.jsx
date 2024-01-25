693e6b290d0a4a8880c84adabab6139601bd8aab
Void <richmondnkrumah04@gmail.com>
2024-01-25 09:25:13 +0000

    removed redundant code

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,70 +1,56 @@
 import { useState } from "react";
 import useStore from "../store";
-import TodoList from "../components/TodoList";
-import Categories from "../components/Categories";
+import Calendar from "react-calendar";
 
 const Dashboard = () => {
-    
+  const getRecentTodos = useStore((state) => state.getRecentTodos);
+  const todos = useStore((state) => state.todos);
 
-    const todos = useStore((state) =>
-        state.todos
-            .filter(
-                (todo) =>
-                    todo.title.includes(searchTerm) &&
-                    (!searchTerm || todo.title.includes(searchTerm))
-            )
-            .filter((todo) => (sortCompleted ? todo.completed : true))
-    );
-   
+  return (
+    <div className="">
+        <div className="flex gap-5">
+       
 
-  
-
-    return (
-        <div>
-            
-            <div className=" grid  grid-cols-2 gap-2">
-                <div className="bg-amber-300 rounded-xl w-[120px] h-32 flex pl-[10%] flex-col justify-end pb-[10%] gap-1">
-                    <p>{todos.length}</p>
-                    <h2 className="text-[12px]">
-                        Project<br></br>Total
-                    </h2>
-                </div>
-                <div className="bg-amber-300 rounded-xl w-[120px] h-32 flex pl-[10%] flex-col justify-end pb-[10%] gap-1">
-                    <p>{todos.filter((todo) => todo.completed).length}</p>
-                    <h2 className="text-[12px]">
-                        Project<br></br>Completed
-                    </h2>
-                </div>
-                <div className="bg-amber-300 rounded-xl w-[120px] h-32 flex pl-[10%] flex-col justify-end pb-[10%] gap-1">
-                    <p>{todos.filter((todo) => todo.completed).length}</p>
-                    <h2 className="text-[12px]">
-                        Tasks<br></br>Total
-                    </h2>
-                </div>
-                <div className="bg-amber-300 rounded-xl w-[120px] h-32 flex pl-[10%] flex-col justify-end pb-[10%] gap-1">
-                    <p>{todos.filter((todo) => !todo.completed).length}</p>
-                    <h2 className="text-[12px]">
-                        Tasks<br></br>Completed
-                    </h2>
-                </div>
-            </div>
-            <div>
-                <div>
-                   {
-                    getRecentTodos().map(todo => (
-                        <div>
-                            {todo.title}
-                        </div>
-                    ))
-                   }
-                </div>
-                {/* <Categories /> */}
-              
-               
-                
-            </div>
+      <div className=" grid  grid-cols-2 gap-2 sm:flex ">
+        <div className="bg-amber-300 rounded-xl w-[120px] sm:w-[250px] h-32 sm:h-[250px] flex pl-4 flex-col justify-end pb-4 gap-1">
+          <p>{todos.length}</p>
+          <h2 className="text-[12px]">
+            Project<br></br>Total
+          </h2>
         </div>
-    );
+        <div className="bg-amber-300 rounded-xl w-[120px] sm:w-[250px] h-32 sm:h-[250px] flex pl-4 flex-col justify-end pb-4 gap-1">
+          <p>{todos.filter((todo) => todo.completed).length}</p>
+          <h2 className="text-[12px]">
+            Project<br></br>Completed
+          </h2>
+        </div>
+        <div className="bg-amber-300 rounded-xl w-[120px] sm:w-[250px] h-32 sm:h-[250px] flex pl-4 flex-col justify-end pb-4 gap-1">
+          <p>{todos.length}</p>
+          <h2 className="text-[12px]">
+            Tasks<br></br>Total
+          </h2>
+        </div>
+        <div className="bg-amber-300 rounded-xl w-[120px] sm:w-[250px] h-32 sm:h-[250px] flex pl-4 flex-col justify-end pb-4 gap-1">
+          <p>{todos.filter((todo) => todo.completed).length}</p>
+          <h2 className="text-[12px]">
+            Tasks<br></br>Completed
+          </h2>
+        </div>
+      </div>
+      <div>
+        <Calendar />
+      </div>
+      </div>
+      <div>
+        <div>
+          {getRecentTodos().map((todo) => (
+            <div>{todo.title}</div>
+          ))}
+        </div>
+        {/* <Categories /> */}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -2,116 +2,67 @@ import React from "react";
 import { useState } from "react";
 import useStore from "../store";
 import TodoList from "../components/TodoList";
-
+import TodoModal from "../components/TodoModal";
 
 const Tasks = () => {
-    const [searchTerm, setSearchTerm] = useState("");
-    const handleSearch = (e) => setSearchTerm(e.target.value);
-    const [sortCompleted, setSortCompleted] = useState(false);
+  const [isOpen,setIsOpen] = useState(false)
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSearch = (e) => setSearchTerm(e.target.value);
+  const [sortCompleted, setSortCompleted] = useState(false);
 
 
-    const [newTodoTitle, setNewTodoTitle] = useState("");
-    const [selectedCategory, setSelectedCategory] = useState("");
-    const [newTags, setNewTags] = useState("");
+  
+  const toggleTodo = useStore((state) => state.toggleTodo);
 
-    const addTodo = useStore((state) => state.addTodo);
-    const deleteTodo = useStore((state) => state.deleteTodo);
-    const toggleTodo = useStore((state) => state.toggleTodo);
-    const getRecentTodos = useStore((state) => state.getRecentTodos)
-    const categories = useStore((state) => state.categories);
-
-    const todos = useStore((state) =>
-        state.todos
-            .filter(
-                (todo) =>
-                    todo.title.includes(searchTerm) &&
-                    (!searchTerm || todo.title.includes(searchTerm))
-            )
-            .filter((todo) => (sortCompleted ? todo.completed : true))
-    );
-    const handleSortCompleted = () => setSortCompleted(!sortCompleted);
-    const handleAddTodo = () => {
-      if (newTodoTitle.trim()) {
-          addTodo(
-              newTodoTitle,
-              selectedCategory,
-              newTags.split(",").map((tag) => tag.trim())
-          );
-          setNewTodoTitle("");
-          setSelectedCategory("");
-          setNewTags("");
-      }
-  };
+  const todos = useStore((state) =>
+    state.todos
+      .filter(
+        (todo) =>
+          todo.title.includes(searchTerm) &&
+          (!searchTerm || todo.title.includes(searchTerm))
+      )
+      .filter((todo) => (sortCompleted ? todo.completed : true))
+  );
+  const handleSortCompleted = () => setSortCompleted(!sortCompleted);
+ 
 
   const handleDeleteTodo = (id) => deleteTodo(id);
   const handleToggleTodo = (id) => toggleTodo(id);
 
-    return (
-        <div>
-            <h1>Tasks</h1>
-            <div>
-                <input
-                    type="text"
-                    placeholder="Search todos..."
-                    value={searchTerm}
-                    onChange={handleSearch}
-                    className=" outline-none mb-3 mt-3 border rounded-full indent-4 h-8"
-                />
-            </div>
-            <div>
-                    <label>
-                        Sort Completed
-                        <input
-                            type="checkbox"
-                            checked={sortCompleted}
-                            onChange={handleSortCompleted}
-                        />
-                    </label>
-                </div>
-                <div>
-                    <h2>Add Todo</h2>
-                    <label>
-                        Title:
-                        <input
-                            type="text"
-                            value={newTodoTitle}
-                            onChange={(e) => setNewTodoTitle(e.target.value)}
-                        />
-                    </label>
-                    <label>
-                        Category:
-                        <select
-                            value={selectedCategory}
-                            onChange={(e) =>
-                                setSelectedCategory(e.target.value)
-                            }
-                        >
-                            <option value="">Select Category</option>
-                            {categories.map((category) => (
-                                <option key={category.id} value={category.id}>
-                                    {category.name}
-                                </option>
-                            ))}
-                        </select>
-                    </label>
-                    <label>
-                        Tags:
-                        <input
-                            type="text"
-                            value={newTags}
-                            onChange={(e) => setNewTags(e.target.value)}
-                        />
-                    </label>
-                    <button onClick={handleAddTodo}>Add Todo</button>
-                </div>
-            <TodoList
-                    todos={todos}
-                    categories={categories}
-                    onDelete={handleDeleteTodo}
-                    onToggle={handleToggleTodo}
-                />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Search todos..."
+          value={searchTerm}
+          onChange={handleSearch}
+          className=" outline-none mb-3 mt-3 border rounded-full indent-4 h-8"
+        />
+      </div>
+      <div>
+        <label>
+          Sort Completed
+          <input
+            type="checkbox"
+            checked={sortCompleted}
+            onChange={handleSortCompleted}
+          />
+        </label>
+      </div>
+     
+      <TodoList
+        todos={todos}
+        onDelete={handleDeleteTodo}
+        onToggle={handleToggleTodo}
+      />
+      <div onClick={() => setIsOpen(state => !state)} className="absolute right-10 cursor-pointer bottom-10 w-10 h-10 bg-purple-800 text-center text-white text-3xl rounded-lg">
+        +
+      </div>
+      <TodoModal isOpen={isOpen} handleOpenModal={() => setIsOpen(false)}/>
+    </div> 
+  );
 };
 
 export default Tasks;
