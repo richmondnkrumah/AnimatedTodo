03ab2bff41c6bb5847d747462181df391a37b0b1
Void <richmondnkrumah04@gmail.com>
2024-01-25 09:26:02 +0000

    just some boilerplate code

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Analytics = () => {
   return (
-    <div>Analytics</div>
+    <div>Analytics
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis harum dolor, consequatur odit commodi dignissimos, officiis non neque nisi nesciunt ab quibusdam voluptate magnam accusamus odio nulla! Nulla, delectus? Quam?</p>
+    </div>
   )
 }
 
