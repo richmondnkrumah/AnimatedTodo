f1cdd8eaff7b0387f9a9072bc04fa803feaeb158
Void <richmondnkrumah04@gmail.com>
2024-01-28 23:12:01 +0000

    integrated chart UI component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,64 +1,108 @@
 import { useState } from "react";
 import useStore from "../store";
 import Calendar from "react-calendar";
-import { TickIcon, CircularTickIcon, GreenCircularTickIcon } from "../assets/icons";
+import {
+  TickIcon,
+  CircularTickIcon,
+  GreenCircularTickIcon,
+  TickSquareIcon,
+  WriteSqaureIcon,
+  PenSquareIcon,
+} from "../assets/icons";
 import { v4 as uniqueID } from "uuid";
+import Dashboardchart from "../components/Dashboardchart";
+import ProjectCard from "../components/ProjectCard";
 
 const Dashboard = () => {
   const getRecentTodos = useStore((state) => state.getRecentTodos);
   const todos = useStore((state) => state.todos);
 
   return (
-    <div className="flex gap-5">
+    <div className="flex sm:flex-row flex-col gap-3 sm:gap-5 h-full">
       <div className="flex flex-col gap-5">
-        <div className=" grid  grid-cols-2 gap-2 sm:flex ">
-          <div className="bg-amber-300 rounded-xl w-[120px] sm:w-[250px] h-32 sm:h-[250px] flex pl-4 flex-col justify-end pb-4 gap-1">
-            <p>{todos.length}</p>
-            <h2 className="text-[12px]">
+        <div>
+          <h2 className=" text-[1em] sm:text-[2em]">Overview</h2>
+        </div>
+        <div className="flex flex-col gap-1  sm:flex-row sm:gap-4">
+          <div className="flex flex-col gap-1 sm:gap-3">
+            <p>Projects</p>
+            <div className="flex justify-between sm:gap-4">
+
+          <div className="bg-amber-300 rounded-xl w-[140px] sm:w-[220px] h-32 sm:h-[220px] flex p-6 flex-col  justify-between  ">
+            <svg className="w-9 h-9 mb-9 rotate-90">{WriteSqaureIcon}</svg>
+            <p className="text-3xl ml-1" >{todos.length}</p>
+            <h2 className=" text-[12px]">
               Project<br></br>Total
             </h2>
           </div>
-          <div className="bg-amber-300 rounded-xl w-[120px] sm:w-[250px] h-32 sm:h-[250px] flex pl-4 flex-col justify-end pb-4 gap-1">
-            <p>{todos.filter((todo) => todo.completed).length}</p>
+          <div className="bg-amber-300 rounded-xl w-[140px] sm:w-[220px] h-32 sm:h-[220px] flex p-6 flex-col justify-between">
+          <svg className="w-9 h-9 mb-9">{TickSquareIcon}</svg>
+            <p className="text-3xl ml-1">{todos.filter((todo) => todo.completed).length}</p>
             <h2 className="text-[12px]">
               Project<br></br>Completed
             </h2>
           </div>
-          <div className="bg-amber-300 rounded-xl w-[120px] sm:w-[250px] h-32 sm:h-[250px] flex pl-4 flex-col justify-end pb-4 gap-1">
-            <p>{todos.length}</p>
-            <h2 className="text-[12px]">
+          </div>
+          </div>
+          <div className="flex flex-col gap-1 sm:gap-3">
+            <p>Tasks</p>
+            <div className="flex justify-between sm:gap-4"> 
+
+          <div className="bg-amber-300 rounded-xl w-[140px] sm:w-[220px] h-32 sm:h-[220px] flex p-6 flex-col justify-between ">
+          <svg className="w-9 h-9 mb-9">{PenSquareIcon}</svg>
+            <p className="text-3xl ml-1" >{todos.length}</p>
+            <h2 className=" text-[12px]">
               Tasks<br></br>Total
             </h2>
           </div>
-          <div className="bg-amber-300 rounded-xl w-[120px] sm:w-[250px] h-32 sm:h-[250px] flex pl-4 flex-col justify-end pb-4 gap-1">
-            <p>{todos.filter((todo) => todo.completed).length}</p>
+          <div className="bg-amber-300 rounded-xl w-[140px] sm:w-[220px] h-32 sm:h-[220px] flex p-6 flex-col justify-between">
+          <svg className="w-9 h-9 mb-9">{TickSquareIcon}</svg>
+            <p className="text-3xl ml-1">{todos.filter((todo) => todo.completed).length}</p>
             <h2 className="text-[12px]">
               Tasks<br></br>Completed
             </h2>
           </div>
+          </div>
+          </div>
+
+
         </div>
-        <div className="flex flex-col sm:flex-row gap-3">
-          <div className="bg-amber-300 rounded-3xl p-4 flex grow flex-col gap-3">
+        <div className="flex flex-col h-full sm:flex-row gap-3">
+          <div className=" flex-grow h-full ">
+            <p className="py-4">Tasks Done</p>
+            <Dashboardchart />
+          </div>
+          <div className="sm:w-[40%] flex bg-amber-300 rounded-3xl p-4 flex-col gap-2">
             <h2>Recent Tasks</h2>
             {getRecentTodos().map((todo, index) => (
-              <div key={uniqueID()} className={`flex ${todo.completed && "line-through" }  gap-3 relative `}>
+              <div key={uniqueID()} className={`flex gap-3 relative `}>
                 <span>{String(index + 1).padStart(2, "0")}</span>
-                <p className=" w-40 sm:w-full overflow-ellipsis overflow-hidden whitespace-nowrap text-nowrap ">{todo.title}</p>
+                <p
+                  className={` ${
+                    todo.completed && "line-through"
+                  } w-40 sm:w-full overflow-ellipsis overflow-hidden whitespace-nowrap text-nowrap `}
+                >
+                  {todo.title}
+                </p>
                 <svg className="w-6 h-6 absolute right-0">
                   {!todo.completed ? CircularTickIcon : GreenCircularTickIcon}
                 </svg>
               </div>
             ))}
           </div>
-          <div className="w-[30%] rounded-3xl h-full bg-amber-300 "></div>
           {/* <Categories /> */}
         </div>
       </div>
-      <div className="hidden sm:flex-col sm:gap-5 sm:flex">
-        <Calendar />
-        <div className="w-full rounded-3xl grow p-5 bg-amber-300 ">
-          <h2>Recent Project</h2>
+      <div className="flex-col w-full gap-2  sm:pt-16 sm:gap-3 flex">
+        {/* <Calendar /> */}
+        <div>
+          <h2 className="">Recent Projects</h2>
         </div>
+        <div className="flex grow gap-5 flex-col justify-between">
+                  <ProjectCard />
+                  <ProjectCard />
+        </div>
+
       </div>
     </div>
   );
