120ea35c75b9eec1bf8700fc11900d2945f51892
Void <richmondnkrumah04@gmail.com>
2024-01-29 20:52:33 +0000

    added tasks details component

---
+++ src/components/TaskDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TaskDetails = () => {
+  return (
+    <div className='p-4'>TaskDetails</div>
+  )
+}
+
+export default TaskDetails
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -3,15 +3,17 @@ import { useState } from "react";
 import useStore from "../store";
 import TodoList from "../components/TodoList";
 import TodoModal from "../components/TodoModal";
+import { FilterIcon, SearchIcon } from "../assets/icons";
+import TaskDetails from "../components/TaskDetails";
 
 const Tasks = () => {
-  const [isOpen,setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const handleSearch = (e) => setSearchTerm(e.target.value);
   const [sortCompleted, setSortCompleted] = useState(false);
 
+  const [currTask,setCurrTask] = useState({})
 
-  
   const toggleTodo = useStore((state) => state.toggleTodo);
   const deleteTodo = useStore((state) => state.deleteTodo);
 
@@ -25,23 +27,36 @@ const Tasks = () => {
       .filter((todo) => (sortCompleted ? todo.completed : true))
   );
   const handleSortCompleted = () => setSortCompleted(!sortCompleted);
- 
 
   const handleDeleteTodo = (id) => deleteTodo(id);
   const handleToggleTodo = (id) => toggleTodo(id);
 
   return (
-    <div>
-      <h1>Tasks</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="Search todos..."
-          value={searchTerm}
-          onChange={handleSearch}
-          className=" outline-none mb-3 mt-3 border rounded-full indent-4 h-8"
-        />
-      </div>
+    <div className="flex h-full gap-5">
+      <div className="grow flex flex-col gap-5">
+        <div className="flex items-center justify-between ">
+          <h1 className=" text-[1em] sm:text-[2em]">All Tasks</h1>
+          <div className="flex items-center relative gap-3">
+            <svg className="w-5 h-5 absolute left-1.5">{SearchIcon}</svg>
+            <input
+              type="text"
+              placeholder="Search tasks"
+              value={searchTerm}
+              onChange={handleSearch}
+              className=" w-80 outline-none  border rounded-full indent-7 h-10 pr-4"
+            />
+            <div>
+              <svg className="w-8 h-8 rotate-90">{FilterIcon}</svg>
+            </div>
+            <div
+              onClick={() => setIsOpen(true)}
+              className=" cursor-pointer bottom-5 w-10 h-10 bg-purple-800 text-center text-white text-3xl rounded-full"
+            >
+              +
+            </div>
+          </div>
+        </div>
+        {/* 
       <div>
         <label>
           Sort Completed
@@ -51,18 +66,19 @@ const Tasks = () => {
             onChange={handleSortCompleted}
           />
         </label>
+      </div> */}
+
+        <TodoList
+          todos={todos}
+          onDelete={handleDeleteTodo}
+          onToggle={handleToggleTodo}
+        />
       </div>
-     
-      <TodoList
-        todos={todos}
-        onDelete={handleDeleteTodo}
-        onToggle={handleToggleTodo}
-      />
-      <div onClick={() => setIsOpen(true)} className="absolute right-5 cursor-pointer bottom-5 w-10 h-10 bg-purple-800 text-center text-white text-3xl rounded-lg">
-        +
+      <div className=" w-[350px] h-full bg-amber-500">
+        <TaskDetails task={currTask}/>
       </div>
-      <TodoModal isOpen={isOpen} handleCloseModal={() => setIsOpen(false)}/>
-    </div> 
+      <TodoModal isOpen={isOpen} handleCloseModal={() => setIsOpen(false)} />
+    </div>
   );
 };
 
