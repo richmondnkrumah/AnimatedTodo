924f9668f59218fc870174ccb82dd332904f2aa2
Void <richmondnkrumah04@gmail.com>
2024-02-04 05:06:13 +0000

    fixed ui responsiveness

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,17 +2,17 @@
 import React from "react";
 import { DeleteIcon } from "../assets/icons";
 
-const Todo = ({ todo, onDelete, onToggle,onCurrentTask }) => {
+const Todo = ({ todo, onDelete, onToggle,isProjectDetail }) => {
   return (
     <div className="flex justify-between items-center">
-      <div className="flex gap-3">
+      <div className="flex gap-3 ">
       <input
-        className=" scale-125 checked:accent-amber-500 "
+        className=" scale-125 checked:accent-[#e60b09] "
         type="checkbox"
         checked={todo.completed}
         onChange={() => onToggle(todo.id)}
       />
-      <span className="cursor-pointer hover:underline" onClick={() => onCurrentTask(todo)}>{todo.title}</span>
+      <span className="cursor-pointer hover:underline">{todo.title}</span>
         
       </div>
       {/* {todo.tags && (
@@ -20,8 +20,12 @@ const Todo = ({ todo, onDelete, onToggle,onCurrentTask }) => {
           Tags: {todo.tags.map((tag) => `#${tag.name} `)}
         </span>
       )} */}
+      
       <div className="flex gap-8">
-        <div>
+      {
+        !isProjectDetail && (
+          <>
+          <div>
           <p>{todo.completed ? "Completed" : "In Progress"}</p>
         </div>
         {todo.Priority === "High" ? (
@@ -37,13 +41,14 @@ const Todo = ({ todo, onDelete, onToggle,onCurrentTask }) => {
             Low
           </span>
         )}
-        <div  className=" w-16 flex items-center justify-center">
+        </>)
+      }
+        
+        <div  className={`w-16 flex items-center ${!isProjectDetail ? "justify-center": "justify-end"} `}>
         <svg  onClick={() => onDelete(todo.id)} className=" cursor-pointer w-5 h-5 ">
           {DeleteIcon}
         </svg>
         </div>
-
-        
       </div>
     </div>
   );
