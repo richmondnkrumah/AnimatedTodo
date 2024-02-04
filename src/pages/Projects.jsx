2f15d160afed2b8cb9d9ee3a1175e98e13659daf
Void <richmondnkrumah04@gmail.com>
2024-02-04 05:06:36 +0000

    added project details and modal components

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,35 +1,33 @@
 import React from "react";
 import { useState } from "react";
 import useStore from "../store";
-import TodoList from "../components/TodoList";
-import TodoModal from "../components/TodoModal";
 import { SearchIcon } from "../assets/icons";
 import ProjectList from "../components/ProjectList";
-
+import ProjectDetails from "../components/ProjectDetails";
+import ProjectModal from "../components/ProjectModal";
 const Projects = () => {
- 
-
   const [searchTerm, setSearchTerm] = useState("");
   const handleSearch = (e) => setSearchTerm(e.target.value);
-
+  const [isOpen, setIsOpen] = useState(false);
 
   const deleteProject = useStore((state) => state.deleteProject);
 
   const projects = useStore((state) =>
-    state.projects
-      .filter(
-        (todo) =>
-          todo.title.includes(searchTerm) &&
-          (!searchTerm || todo.title.includes(searchTerm))
-      )
+    state.projects.filter(
+      (project) =>
+        project.title.includes(searchTerm) &&
+        (!searchTerm || project.title.includes(searchTerm))
+    )
   );
-  
   const handleDeleteProject = (id) => deleteProject(id);
-  
-  console.log(projects)
+  const [currProject, setCurrProject] = useState(null);
+
+  const handleCurrentProjectDetails = (projectID) => {
+    setCurrProject(projectID);
+  };
   return (
-    <div className="flex h-full w-fit">
-      <div className="grow flex flex-col gap-5">
+    <div className="flex h-full ">
+      <div className=" grow flex flex-col gap-5 mr-[280px] pr-5">
         <div className="flex items-center justify-between ">
           <h1 className=" text-[1em] sm:text-[2em]">Projects</h1>
           <div className="flex items-center relative gap-3">
@@ -43,21 +41,28 @@ const Projects = () => {
             />
             <div
               onClick={() => setIsOpen(true)}
-              className=" cursor-pointer bottom-5 w-10 h-10 bg-purple-800 text-center text-white text-3xl rounded-full"
+              className=" cursor-pointer bottom-5 w-10 h-10 bg-[#e60b09] text-center text-white text-3xl rounded-full"
             >
               +
             </div>
           </div>
         </div>
-         <ProjectList projects={projects} onDelete={handleDeleteProject}/>
-        
-      </div>
-      <div>
 
+        <ProjectList
+          projects={projects}
+          onDelete={handleDeleteProject}
+          onCurrentProject={handleCurrentProjectDetails}
+        />
       </div>
-
+        <div className="fixed w-[280px] right-5 top-5 h-[calc(100%_-_40px)] bg-[#171717] text-white rounded-3xl">
+          <ProjectDetails
+            currProject={projects.find((project) => project.id === currProject)}
+            handleDeleteProject={handleDeleteProject}
+            />
+            </div>
+      <ProjectModal isOpen={isOpen} handleCloseModal={() => setIsOpen(false)} />
     </div>
   );
-}
+};
 
-export default Projects
+export default Projects;
