183043ff771af47fa11e36f9454e67ecde6b6430
Void <richmondnkrumah04@gmail.com>
2024-02-04 05:04:30 +0000

    changed UI colors

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,67 +15,83 @@ import ProjectCard from "../components/ProjectCard";
 
 const Dashboard = () => {
   const getRecentTodos = useStore((state) => state.getRecentTodos);
+  const getRecentProjects = useStore((state) => state.getRecentProjects);
   const todos = useStore((state) => state.todos);
+  const projects = useStore((state) => state.projects);
 
   return (
-    <div className="flex sm:flex-row flex-col  gap-3 justify-between h-full">
+    <div className="flex sm:flex-row flex-col gap-3 justify-between h-full">
       <div className="flex flex-col gap-5">
         <div>
-          <h2 className=" text-[1em] sm:text-[2em]">Overview</h2>
+          <h2 className=" text-[1em] sm:text-[2em] text-white">Overview</h2>
         </div>
-        <div className="flex flex-col gap-1  sm:flex-row sm:gap-5">
+        <div className="flex flex-col gap-1 text-white sm:flex-row sm:gap-5">
           <div className="flex flex-col gap-1 sm:gap-3">
             <p>Projects</p>
             <div className="flex justify-between sm:gap-5">
+              <div className="bg-[#171717] rounded-xl w-[140px] sm:w-[220px] h-32 sm:h-[220px] flex p-6 flex-col  justify-between ">
+                <div className=" BOX1 w-11 h-11 rounded-xl relative">
+                  <svg className="w-9 h-9 mb-9  rotate-90 absolute top-[50%] -translate-y-[50%] -translate-x-[50%] left-[50%]">
+                    {WriteSqaureIcon}
+                  </svg>
+                </div>
+                <p className="text-3xl ml-1">{projects.length}</p>
+                <h2 className=" text-[12px]">
+                  Project<br></br>Total
+                </h2>
+              </div>
+              <div className="bg-[#171717]  rounded-xl w-[140px] sm:w-[220px] h-32 sm:h-[220px] flex p-6 flex-col justify-between">
+              <div className="bg-red-500 BOX4 w-11 h-11 rounded-xl relative">
 
-          <div className="bg-amber-300 rounded-xl w-[140px] sm:w-[220px] h-32 sm:h-[220px] flex p-6 flex-col  justify-between  ">
-            <svg className="w-9 h-9 mb-9 rotate-90">{WriteSqaureIcon}</svg>
-            <p className="text-3xl ml-1" >{todos.length}</p>
-            <h2 className=" text-[12px]">
-              Project<br></br>Total
-            </h2>
-          </div>
-          <div className="bg-amber-300 rounded-xl w-[140px] sm:w-[220px] h-32 sm:h-[220px] flex p-6 flex-col justify-between">
-          <svg className="w-9 h-9 mb-9">{TickSquareIcon}</svg>
-            <p className="text-3xl ml-1">{todos.filter((todo) => todo.completed).length}</p>
-            <h2 className="text-[12px]">
-              Project<br></br>Completed
-            </h2>
-          </div>
-          </div>
+                <svg className="w-9 h-9 mb-9 absolute top-[50%] -translate-y-[50%] -translate-x-[50%] left-[50%]">{TickSquareIcon}</svg>
+</div>
+                <p className="text-3xl ml-1">
+                  {projects.filter((project) => project.completed).length}
+                </p>
+                <h2 className="text-[12px]">
+                  Project<br></br>Completed
+                </h2>
+              </div>
+            </div>
           </div>
           <div className="flex flex-col gap-1 sm:gap-3">
             <p>Tasks</p>
-            <div className="flex justify-between sm:gap-5"> 
-
-          <div className="bg-amber-300 rounded-xl w-[140px] sm:w-[220px] h-32 sm:h-[220px] flex p-6 flex-col justify-between ">
-          <svg className="w-9 h-9 mb-9">{PenSquareIcon}</svg>
-            <p className="text-3xl ml-1" >{todos.length}</p>
-            <h2 className=" text-[12px]">
-              Tasks<br></br>Total
-            </h2>
+            <div className="flex justify-between sm:gap-5">
+              <div className="bg-[#171717] rounded-xl w-[140px] sm:w-[220px] h-32 sm:h-[220px] flex p-6 flex-col justify-between ">
+              <div className=" BOX3 w-11 h-11 rounded-xl relative">
+                
+                <svg className="w-9 h-9 mb-9 absolute top-[50%] -translate-y-[50%] -translate-x-[50%] 
+                left-[50%]">{PenSquareIcon}</svg>
+                </div>
+                <p className="text-3xl ml-1">{todos.length}</p>
+                <h2 className=" text-[12px]">
+                  Tasks<br></br>Total
+                </h2>
+              </div>
+              <div className="bg-[#171717] rounded-xl w-[140px] sm:w-[220px] h-32 sm:h-[220px] flex p-6 flex-col justify-between">
+              <div className=" BOX4 w-11 h-11 rounded-xl relative">
+                
+                <svg className="w-9 h-9 mb-9 absolute top-[50%] -translate-y-[50%] -translate-x-[50%] left-[50%]">{TickSquareIcon}</svg>
+                </div>
+                <p className="text-3xl ml-1">
+                  {todos.filter((todo) => todo.completed).length}
+                </p>
+                <h2 className="text-[12px]">
+                  Tasks<br></br>Completed
+                </h2>
+              </div>
+            </div>
           </div>
-          <div className="bg-amber-300 rounded-xl w-[140px] sm:w-[220px] h-32 sm:h-[220px] flex p-6 flex-col justify-between">
-          <svg className="w-9 h-9 mb-9">{TickSquareIcon}</svg>
-            <p className="text-3xl ml-1">{todos.filter((todo) => todo.completed).length}</p>
-            <h2 className="text-[12px]">
-              Tasks<br></br>Completed
-            </h2>
-          </div>
-          </div>
-          </div>
-
-
         </div>
         <div className="flex flex-col h-full sm:flex-row gap-3">
-          <div className=" flex-grow h-full ">
+          <div className=" flex-grow h-full text-white ">
             <p className="py-4">Tasks Done</p>
             <Dashboardchart />
           </div>
-          <div className="sm:w-[40%] flex bg-amber-300 rounded-3xl p-4 flex-col gap-2">
+          <div className="sm:w-[40%] flex bg-[#171717] text-white rounded-3xl p-4 flex-col gap-3">
             <h2>Recent Tasks</h2>
             {getRecentTodos().map((todo, index) => (
-              <div key={uniqueID()} className={`flex gap-3 relative `}>
+              <div key={uniqueID()} className="flex gap-3 relative RecentTasks">
                 <span>{String(index + 1).padStart(2, "0")}</span>
                 <p
                   className={` ${
@@ -93,15 +109,15 @@ const Dashboard = () => {
           {/* <Categories /> */}
         </div>
       </div>
-      <div className="flex-col w-fit gap-2    sm:pt-16 sm:gap-3 flex">
+      <div className="flex-col w-fit gap-2  text-white sm:pt-16 sm:gap-3 flex">
         <div>
           <h2 className="">Recent Projects</h2>
         </div>
         <div className="flex grow gap-5 flex-col justify-between">
-                  <ProjectCard />
-                  <ProjectCard />
+          {getRecentProjects().map((project) => (
+            <ProjectCard project={project} />
+          ))}
         </div>
-
       </div>
     </div>
   );
