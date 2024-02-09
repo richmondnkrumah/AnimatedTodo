79b0f40ab66df9b8d06a35a8dbc34f1a92fa6fb0
Void <richmondnkrumah04@gmail.com>
2024-02-09 04:09:41 +0000

    handled colour icon states navigation

--- src/components/NavSidebar.jsx
+++ src/components/NavSidebar.jsx
@@ -8,16 +8,23 @@ import {
     AnalyticsIcon,
     SettingIcon,
 } from "../assets/icons";
+import useStore from "../store";
 
 const NavSidebar = ({handleSectionChange,currentSection}) => {
+    const theme = useStore(state => state.theme)
+    const changeTheme = useStore(state => state.changeTheme)
     const [darkMode, setDarkMode] = useState(false);
     
     if (darkMode) {
       document.documentElement.classList.add('dark')
+      changeTheme("dark")
     }
     else {
       document.documentElement.classList.remove('dark')
+      changeTheme("light")
+
     }
+    console.log(theme)
 
     return (
         <motion.nav className="fixed top-0 bg-[#171717] h-full w-[50px] sm:w-[220px] dark:bg-purple-500 text-white">
@@ -30,19 +37,19 @@ const NavSidebar = ({handleSectionChange,currentSection}) => {
                         onClick={() => handleSectionChange("Dashboard")}
                         className={` ${currentSection === "Dashboard" ? "text-white" : "text-[#495057]"} flex gap-2 items-center cursor-pointer`}
                     >
-                        <svg className="w-8 h-8">{DashboardIcon}</svg>
+                        <svg className="w-8 h-8">{DashboardIcon(currentSection === "Dashboard")}</svg>
                         <p className="hidden sm:block">Dashboard</p>
                     </div>
                     <div onClick={() => handleSectionChange('Tasks')} className={` ${currentSection === "Tasks" ? "text-white" : "text-[#495057]"} flex gap-2 items-center cursor-pointer`}>
-                        <svg className="w-8 h-8">{TasksIcon}</svg>
+                        <svg className="w-8 h-8">{TasksIcon(currentSection === "Tasks")}</svg>
                         <p className="hidden sm:block">Tasks</p>
                     </div>
                     <div onClick={() => handleSectionChange('Projects')} className={` ${currentSection === "Projects" ? "text-white" : "text-[#495057]"} flex gap-2 items-center cursor-pointer`}>
-                        <svg className="w-8 h-8">{SettingIcon}</svg>
+                        <svg className="w-8 h-8">{SettingIcon(currentSection === "Projects")}</svg>
                         <p className="hidden sm:block">Projects</p>
                     </div>
                     <div onClick={() => handleSectionChange('Analytics')} className={` ${currentSection === "Analytics" ? "text-white" : "text-[#495057]"} flex gap-2 items-center cursor-pointer`}>
-                        <svg className="w-8 h-8">{AnalyticsIcon}</svg>
+                        <svg className="w-8 h-8">{AnalyticsIcon(currentSection === "Analytics")}</svg>
                         <p className="hidden sm:block">Analytics</p>
                     </div>
                     
